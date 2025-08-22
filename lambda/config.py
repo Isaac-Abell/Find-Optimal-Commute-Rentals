9aaaefc8b002a7870b0af05731b2c9a74655b81c
Isaac-A-UofT <isaac.abell@mail.utoronto.ca>
2025-08-22 12:37:51 -0700

    fix

--- lambda/config.py
+++ lambda/config.py
@@ -17,9 +17,9 @@ MAX_DISTANCE_MILES = 50
 
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_PASSWORD = os.environ.get("password")
 DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
+DB_NAME = os.environ.get("username")
 DB_PORT = os.environ.get("DB_PORT", "5432")
 
 # --- SQLAlchemy table definition ---
