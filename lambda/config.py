f688c98f109aa586caf322e525c7377048b481a4
Isaac-A-UofT <isaac.abell@mail.utoronto.ca>
2025-08-25 21:53:57 -0700

    add more cities

--- lambda/config.py
+++ lambda/config.py
@@ -10,7 +10,14 @@ CITY_CENTERS = {
     "Austin, TX": (30.2672, -97.7431),
     "Boston, MA": (42.3601, -71.0589),
     "Denver, CO": (39.7392, -104.9903),
-    "Los Angeles, CA": (34.0522, -118.2437)
+    "Los Angeles, CA": (34.0522, -118.2437),
+    "Miami, FL": (25.7617, -80.1918),
+    "Chicago, IL": (41.8781, -87.6298),
+    "Washington, D.C.": (38.9072, -77.0369),
+    "Phoenix, AZ": (33.4484, -112.0740),
+    "Philadelphia, PA": (39.9526, -75.1652),
+    "Atlanta, GA": (33.7490, -84.3880),
+    "Salt Lake City, UT": (40.7608, -111.8910)
 }
 
 MAX_DISTANCE_KM = 50
